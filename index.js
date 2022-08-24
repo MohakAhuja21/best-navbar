29c9233949e55ff6680a7e96a42e02558ca90dcb
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-08-24 19:36:42 +0530

    added upto desktop-nav

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-// calling elements
+// calling html elem
+const loginForm=document.querySelector('.login-popup');
 const loginBtn=document.querySelector('.login-btn');
-const loginForm=document.querySelector('.login-form');
-const loginFormCloseBtn=document.querySelector('.form i');
-const opacity=document.querySelector('.opacity');
-const searchPopup=document.querySelector('.search-form');
-const searchBar=document.getElementById('search');
-const closeSearchPopup=document.querySelector('.search-form i');
+const closeBtn=document.querySelector('.login-form i');
+const overlay=document.querySelector('.overlay');
+const navbar=document.getElementById('navbar');
 
+// manipulating
 loginBtn.addEventListener('click',()=>{
     loginForm.classList.add('active')
-    opacity.classList.add('show')
-    document.getElementById('nav-container').style.pointerEvents="none";
+    overlay.classList.add('show')
+    navbar.style.pointerEvents="none"
+    
 });
-
-loginFormCloseBtn.addEventListener('click',()=>{
+closeBtn.addEventListener('click',()=>{
     loginForm.classList.remove('active')
-    opacity.classList.remove('show')
-    document.getElementById('nav-container').style.pointerEvents="all";
+    overlay.classList.remove('show')
+    navbar.style.pointerEvents="all"
 });
 
-searchBar.addEventListener('mousedown',()=>{
-    searchPopup.classList.add('active')
-    opacity.classList.add('show')
-    document.getElementById('nav-container').style.pointerEvents="none";
-});
-
-closeSearchPopup.addEventListener('click',()=>{
-    searchPopup.classList.remove('active')
-    opacity.classList.remove('show')
-    document.getElementById('nav-container').style.pointerEvents="all";
-});
-
-opacity.addEventListener('click',()=>{
-    searchPopup.classList.remove('active')
-    loginForm.classList.remove('active')
-    document.getElementById('nav-container').style.pointerEvents="all";
-    opacity.classList.remove('show')
-});
 
 
