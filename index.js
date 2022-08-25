6809c613788815704b6853cfbadcd427f9242b8a
Mohak Ahuja <mohakaahuaj@outlook.com>
2022-08-25 13:00:33 +0530

    added upto search bar popup

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 // calling html elem
-const loginForm=document.querySelector('.login-popup');
-const loginBtn=document.querySelector('.login-btn');
-const closeBtn=document.querySelector('.login-form i');
-const overlay=document.querySelector('.overlay');
-const navbar=document.getElementById('navbar');
+const loginForm = document.querySelector(".login-popup");
+const loginBtn = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".login-form i");
+const overlay = document.querySelector(".overlay");
+const navbar = document.getElementById("navbar");
 
 // manipulating
-loginBtn.addEventListener('click',()=>{
-    loginForm.classList.add('active')
-    overlay.classList.add('show')
-    navbar.style.pointerEvents="none"
-    
+loginBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+  overlay.classList.add("show");
+  navbar.style.pointerEvents = "none";
 });
-closeBtn.addEventListener('click',()=>{
-    loginForm.classList.remove('active')
-    overlay.classList.remove('show')
-    navbar.style.pointerEvents="all"
+closeBtn.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+  overlay.classList.remove("show");
+  navbar.style.pointerEvents = "all";
 });
+
+
+
+
 
 
 
